708612619eea1541743631d0cd49d59e062a03e6
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2025-09-03 17:26:08 +0900

    9월 3일 5시 25분 작업
    기본 툴바 생성및 설정

--- PairBoardFrontPJ/src/App.tsx
+++ PairBoardFrontPJ/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { AppBar, Box, Button, Container, CssBaseline, Toolbar, Typography } from '@mui/material'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxWidth='xl'>
+        <CssBaseline />
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant="h4" sx={{mr: 7}} >
+                게시판
+              </Typography>
+            <Box display="flex" alignItems="center" gap={3} sx={{ flexGrow: 1 }}>
+              <Typography variant="h6">공지사항</Typography>
+              <Typography variant="h6">자동차</Typography>
+              <Typography variant="h6">게임</Typography>
+              <Typography variant="h6">영화</Typography>
+              <Typography variant="h6">스포츠</Typography>
+              <Typography variant="h6">만화</Typography>
+            </Box>
+            <Button color="inherit" variant="text" size="large">로그인</Button>
+          </Toolbar>
+        </AppBar>
+      </Container>
     </>
   )
 }
